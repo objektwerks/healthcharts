d75bbd39213223579d72963f2c4525a2642166ca
objektwerks <objektwerks@runbox.com>
2020-06-26 15:21:27 -0400

    refactored line chart and app

--- src/main/scala/chart/App.scala
+++ src/main/scala/chart/App.scala
@@ -2,13 +2,23 @@ package chart
 
 import java.awt.EventQueue
 
+import java.awt.BorderLayout
+import javax.swing.JFrame
+import javax.swing.WindowConstants
+
 object App {
   def main(args: Array[String]): Unit = {
     println("*** App main!")
     try {
       EventQueue.invokeLater( new Runnable() {
         override def run(): Unit = {
-          val frame = LineChart()
+          val frame = new JFrame()
+          val chart = LineChart()
+          frame.add(chart, BorderLayout.CENTER)
+          frame.setTitle("JFreeChart")
+          frame.setSize(640, 480)
+          frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+          frame.setLocationRelativeTo(null)
           frame.setVisible(true);
         }
       })
