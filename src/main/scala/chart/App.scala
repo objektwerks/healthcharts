92ee9856f27a5630a4e426e0a05953c607ea5570
objektwerks <objektwerks@runbox.com>
2020-06-26 15:48:54 -0400

    removed App try/catch and println

--- src/main/scala/chart/App.scala
+++ src/main/scala/chart/App.scala
@@ -8,22 +8,17 @@ import javax.swing.WindowConstants
 
 object App {
   def main(args: Array[String]): Unit = {
-    println("*** App main!")
-    try {
-      EventQueue.invokeLater( new Runnable() {
-        override def run(): Unit = {
-          val chart = LineChart()
-          val frame = new JFrame()
-          frame.add(chart, BorderLayout.CENTER)
-          frame.setTitle("JFreeChart")
-          frame.setSize(640, 480)
-          frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-          frame.setLocationRelativeTo(null)
-          frame.setVisible(true);
-        }
-      })
-    } catch {
-      case t: Throwable => println(s"*** App Error: ${t.getMessage}")
-    }
+    EventQueue.invokeLater( new Runnable() {
+      override def run(): Unit = {
+        val chart = LineChart()
+        val frame = new JFrame()
+        frame.add(chart, BorderLayout.CENTER)
+        frame.setTitle("JFreeChart")
+        frame.setSize(640, 480)
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+        frame.setLocationRelativeTo(null)
+        frame.setVisible(true);
+      }
+    })
   }
 }
