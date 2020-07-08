eeac66c4221dcfb4275f6a127c809c134b6e248a
objektwerks <objektwerks@runbox.com>
2020-07-08 14:31:27 -0400

    added Product and Serializable to MedType

--- src/main/scala/chart/Entity.scala
+++ src/main/scala/chart/Entity.scala
@@ -4,7 +4,7 @@ import java.time.LocalDateTime
 
 final case class Glucose(datetime: LocalDateTime, level: Int)
 
-sealed trait MedType
+sealed trait MedType extends Product with Serializable
 final case object Insulin extends MedType
 final case object Steroid extends MedType
 
