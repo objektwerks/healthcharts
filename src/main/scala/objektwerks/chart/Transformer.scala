059020f7f9d8602827676bacac325e65035afa03
objektwerks <objektwerks@runbox.com>
2020-07-14 14:24:19 -0400

    reformatted transformer

--- src/main/scala/objektwerks/chart/Transformer.scala
+++ src/main/scala/objektwerks/chart/Transformer.scala
@@ -2,12 +2,12 @@ package objektwerks.chart
 
 object Transformer {
   def csvToGlucose(path: String): List[Glucose] = {
-      println(path)
-      List.empty[Glucose]
+    println(path)
+    List.empty[Glucose]
   }
 
   def csvToMeds(path: String): List[Med] = {
-      println(path)
-      List.empty[Med]
+    println(path)
+    List.empty[Med]
   }
 }
