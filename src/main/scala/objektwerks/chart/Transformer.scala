8cf44bc7dd1c40b500e32e212c9b5529bcdc1769
objektwerks <objektwerks@runbox.com>
2020-07-14 15:24:43 -0400

    wrapped public transformer methods in Try

--- src/main/scala/objektwerks/chart/Transformer.scala
+++ src/main/scala/objektwerks/chart/Transformer.scala
@@ -6,30 +6,35 @@ import org.jfree.data.time.Minute
 
 import scala.io.{Codec, Source}
 import scala.collection.mutable
+import scala.util.Try
 
 object Transformer {
   val utf8 = Codec.UTF8.name
 
-  def csvToGlucose(path: String): List[Glucose] = {
-    val buffer = mutable.ArrayBuffer[Glucose]()
-    val lines = csvToLines(path)
-    println(lines)
-    lines.foreach { line =>
-      val columns = line.split(",").map(_.trim)
-      buffer.addOne( Glucose( datetimeToMinute(columns(0)), columns(1).toInt) )
+  def csvToGlucose(path: String): Try[List[Glucose]] = {
+    Try {
+      val buffer = mutable.ArrayBuffer[Glucose]()
+      val lines = csvToLines(path)
+      println(lines)
+      lines.foreach { line =>
+        val columns = line.split(",").map(_.trim)
+        buffer.addOne( Glucose( datetimeToMinute(columns(0)), columns(1).toInt) )
+      }
+      buffer.toList
     }
-    buffer.toList
   }
 
-  def csvToMeds(path: String): List[Med] = {
-    val buffer = mutable.ArrayBuffer[Med]()
-    val lines = csvToLines(path)
-    println(lines)
-    lines.foreach { line =>
-      val columns = line.split(",").map(_.trim)
-      buffer.addOne( Med( datetimeToMinute(columns(0)), MedType.map(columns(1).toInt), columns(2).toInt) )
+  def csvToMeds(path: String): Try[List[Med]] = {
+    Try {
+      val buffer = mutable.ArrayBuffer[Med]()
+      val lines = csvToLines(path)
+      println(lines)
+      lines.foreach { line =>
+        val columns = line.split(",").map(_.trim)
+        buffer.addOne( Med( datetimeToMinute(columns(0)), MedType.map(columns(1).toInt), columns(2).toInt) )
+      }
+      buffer.toList
     }
-    buffer.toList
   }
 
   def csvToLines(path: String): Array[String] = {
