6d627e47753e0009ccf4540ecad6bb039cd77791
objektwerks <objektwerks@runbox.com>
2020-07-14 15:15:39 -0400

    implemented transformer

--- src/main/scala/objektwerks/chart/Transformer.scala
+++ src/main/scala/objektwerks/chart/Transformer.scala
@@ -5,20 +5,31 @@ import java.time.LocalDateTime
 import org.jfree.data.time.Minute
 
 import scala.io.{Codec, Source}
+import scala.collection.mutable
 
 object Transformer {
   val utf8 = Codec.UTF8.name
 
   def csvToGlucose(path: String): List[Glucose] = {
+    val buffer = mutable.ArrayBuffer[Glucose]()
     val lines = csvToLines(path)
     println(lines)
-    List.empty[Glucose]
+    lines.foreach { line =>
+      val columns = line.split(",").map(_.trim)
+      buffer.addOne( Glucose( datetimeToMinute(columns(0)), columns(1).toInt) )
+    }
+    buffer.toList
   }
 
   def csvToMeds(path: String): List[Med] = {
+    val buffer = mutable.ArrayBuffer[Med]()
     val lines = csvToLines(path)
     println(lines)
-    List.empty[Med]
+    lines.foreach { line =>
+      val columns = line.split(",").map(_.trim)
+      buffer.addOne( Med( datetimeToMinute(columns(0)), MedType.map(columns(1).toInt), columns(2).toInt) )
+    }
+    buffer.toList
   }
 
   def csvToLines(path: String): Array[String] = {
