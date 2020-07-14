7f9078a6f8b159879020413995647876aff8f35a
objektwerks <objektwerks@runbox.com>
2020-07-14 14:23:11 -0400

    added transformer

---
+++ src/main/scala/objektwerks/chart/Transformer.scala
@@ -0,0 +1,13 @@
+package objektwerks.chart
+
+object Transformer {
+  def csvToGlucose(path: String): List[Glucose] = {
+      println(path)
+      List.empty[Glucose]
+  }
+
+  def csvToMeds(path: String): List[Med] = {
+      println(path)
+      List.empty[Med]
+  }
+}
