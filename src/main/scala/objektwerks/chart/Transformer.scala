6a9dc9aeac021154b1363ce98bf8fb2c2d74cd76
objektwerks <objektwerks@runbox.com>
2020-07-14 15:34:28 -0400

    refactored transformer Source.fromFile code

--- src/main/scala/objektwerks/chart/Transformer.scala
+++ src/main/scala/objektwerks/chart/Transformer.scala
@@ -14,9 +14,9 @@ object Transformer {
   def csvToGlucose(path: String): Try[List[Glucose]] = {
     Try {
       val buffer = mutable.ArrayBuffer[Glucose]()
-      val lines = csvToLines(path)
-      println(lines)
-      lines.foreach { line =>
+      val source = Source.fromFile(path, utf8)
+      for (line <- source.getLines) {
+        println(line)
         val columns = line.split(",").map(_.trim)
         buffer.addOne( Glucose( datetimeToMinute(columns(0)), columns(1).toInt) )
       }
@@ -27,18 +27,14 @@ object Transformer {
   def csvToMeds(path: String): Try[List[Med]] = {
     Try {
       val buffer = mutable.ArrayBuffer[Med]()
-      val lines = csvToLines(path)
-      println(lines)
-      lines.foreach { line =>
+      val source = Source.fromFile(path, utf8)
+      for (line <- source.getLines) {
+        println(line)
         val columns = line.split(",").map(_.trim)
         buffer.addOne( Med( datetimeToMinute(columns(0)), MedType.map(columns(1).toInt), columns(2).toInt) )
       }
       buffer.toList
     }
-  }
-
-  def csvToLines(path: String): Array[String] = {
-    Source.fromFile(path, utf8).mkString.split("\n")
   }
 
   def datetimeToMinute(datetime: String): Minute = {
