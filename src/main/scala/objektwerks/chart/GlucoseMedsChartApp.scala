efade466fa50e50b5cb8bc37a4e94d8a98409e9b
objektwerks <objektwerks@runbox.com>
2020-07-17 16:11:43 -0400

    added GlucoseMedsChartApp

---
+++ src/main/scala/objektwerks/chart/GlucoseMedsChartApp.scala
@@ -0,0 +1,25 @@
+package objektwerks.chart
+
+import java.awt.BorderLayout
+import java.awt.EventQueue
+
+import javax.swing.JFrame
+import javax.swing.WindowConstants
+
+object GlucoseMedsChartApp {
+  val centerWindow = null
+  def main(args: Array[String]): Unit = {
+    EventQueue.invokeLater( new Runnable() {
+      override def run(): Unit = {
+        val chart = Chart()
+        val frame = new JFrame()
+        frame.add(chart, BorderLayout.CENTER)
+        frame.setTitle("Medical Chart")
+        frame.setSize(640, 480)
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+        frame.setLocationRelativeTo(centerWindow)
+        frame.setVisible(true);
+      }
+    })
+  }
+}
