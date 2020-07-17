d62ae9a1dfd7756e5259d8e3077e30d1844e14e2
objektwerks <objektwerks@runbox.com>
2020-07-17 16:09:00 -0400

    reformatted GlucoseMedsChart

--- src/main/scala/objektwerks/chart/GlucoseMedsChart.scala
+++ src/main/scala/objektwerks/chart/GlucoseMedsChart.scala
@@ -1,6 +1,6 @@
 package objektwerks.chart
 
 final case class GlucoseMedsChart(glucose: Array[Glucose], meds: Array[Med]) {
-    println(glucose.toList.toString)
-    println(meds.toList.toString)
+  println(glucose.toList.toString)
+  println(meds.toList.toString)
  }
