c94b4da5b212fd9bfbb5ca1e02e6eaf8e5064fec
objektwerks <objektwerks@runbox.com>
2020-07-20 10:19:15 -0400

    refactored transformer tests

--- src/test/scala/objektwerks/chart/TransformerTest.scala
+++ src/test/scala/objektwerks/chart/TransformerTest.scala
@@ -13,46 +13,36 @@ class TransformerTest extends AnyFunSuite with Matchers {
   val logger = LoggerFactory.getLogger(this.getClass)
   
   test("glucose") {
-    csvToGlucose("./data/glucose/glucose.txt") match {
-      case Success((lines, errors)) =>
-        lines.length shouldEqual 23
-        errors.isEmpty shouldBe true
-      case Failure(error) =>
-        logger.error(s"glucose test fail: ${error.printStackTrace()}")
-        fail
-    }
-  }
-
-  test("glucose invalid") {
-    csvToGlucose("./data/glucose/glucose-invalid.txt") match {
-      case Success((lines, errors)) =>
-        lines.length shouldEqual 20
-        errors.length shouldEqual 3
-      case Failure(error) =>
-        logger.error(s"glucose test fail: ${error.printStackTrace()}")
-        fail
-    }
-  }
+    csvToGlucoseTest("./data/glucose/glucose.txt", 23, 0)
+    csvToGlucoseTest("./data/glucose/glucose-invalid.txt", 20, 3)
+ }
 
   test("meds") {
-    csvToMeds("./data/meds/meds.txt") match {
-      case Success((lines, errors)) =>
-        lines.length shouldEqual 23
-        errors.isEmpty shouldBe true
-      case Failure(error) => 
-        logger.error(s"meds test fail: ${error.printStackTrace()}")
-        fail
-    }
+    csvToMedsTest("./data/meds/meds.txt", 23, 0)
+    csvToMedsTest("./data/meds/meds-invalid.txt", 20, 3)
   }
 
-  test("meds-invalid") {
-    csvToMeds("./data/meds/meds-invalid.txt") match {
+  private def csvToGlucoseTest(path: String, linesCount: Int, errorsCount: Int): Unit = {
+    csvToGlucose(path) match {
       case Success((lines, errors)) =>
-        lines.length shouldEqual 20
-        errors.length shouldEqual 3
-      case Failure(error) => 
+        lines.length shouldEqual linesCount
+        errors.length shouldEqual errorsCount
+      case Failure(error) =>
+        logger.error(s"glucose test fail: ${error.printStackTrace()}")
+        fail
+    }
+    ()
+  }
+
+  private def csvToMedsTest(path: String, linesCount: Int, errorsCount: Int): Unit = {
+    csvToMeds(path) match {
+      case Success((lines, errors)) =>
+        lines.length shouldEqual linesCount
+        errors.length shouldEqual errorsCount
+      case Failure(error) =>
         logger.error(s"meds test fail: ${error.printStackTrace()}")
         fail
     }
+    ()
   }
 }
