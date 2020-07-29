3599f8e37282e10da3cfe4e89bc63707d9df6c9a
objektwerks <objektwerks@runbox.com>
2020-07-29 07:18:18 -0400

    removed class and type tag tests; moved to project scala

--- src/test/scala/objektwerks/chart/TransformerTest.scala
+++ src/test/scala/objektwerks/chart/TransformerTest.scala
@@ -11,26 +11,6 @@ import Transformer._
 
 class TransformerTest extends AnyFunSuite with Matchers {
   val logger = LoggerFactory.getLogger(this.getClass)
-  
-  test("class tag") {
-    import scala.reflect._
-    def isTypeof[T: ClassTag](list: List[T]): ClassTag[T] = classTag[T] match {
-      case g if g === classTag[Glucose] => println(s"*** glucose class tag: ${list.toString}"); g
-      case m if m === classTag[Med] => println(s"*** med class tag: ${list.toString}"); m
-    }
-    isTypeof(List.empty[Glucose]) === classTag[Glucose] shouldBe true
-    isTypeof(List.empty[Med]) === classTag[Med] shouldBe true
-  }
-
-  test("type tag") {
-    import scala.reflect.runtime.universe._
-    def isTypeof[T: TypeTag](list: List[T]): TypeTag[T] = typeTag[T] match {
-      case g if g === typeTag[Glucose] => println(s"*** glucose type tag: ${list.toString}"); g
-      case m if m === typeTag[Med] => println(s"*** med type tag: ${list.toString}"); m
-    }
-    isTypeof(List.empty[Glucose]) === typeTag[Glucose] shouldBe true
-    isTypeof(List.empty[Med]) === typeTag[Med] shouldBe true
-  }
 
   test("glucose") {
     csvToGlucoseTest("./data/glucose/glucose.txt", 23, 0)
