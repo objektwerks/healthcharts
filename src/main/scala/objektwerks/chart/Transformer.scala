41701af18b6e4760096b70879658f7d80bf85e73
objektwerks <objektwerks@runbox.com>
2020-08-16 08:20:49 -0400

    refactored trasnformer buffered source code

--- src/main/scala/objektwerks/chart/Transformer.scala
+++ src/main/scala/objektwerks/chart/Transformer.scala
@@ -2,41 +2,45 @@ package objektwerks.chart
 
 import org.slf4j.LoggerFactory
 
-import scala.collection.mutable.ArrayBuilder
+import scala.collection.mutable
 import scala.io.{Codec, Source}
 import scala.util.Try
 
 final case class InvalidLine(line: String, error: Throwable)
 
 object Transformer {
-  private val logger = LoggerFactory.getLogger(GlucoseMedsChart.getClass())
+  private val logger = LoggerFactory.getLogger(GlucoseMedsChart.getClass)
   private val utf8 = Codec.UTF8.name
 
   def csvToGlucose(path: String, delimiter: String = ","): Try[(Array[Glucose], Array[InvalidLine])] =
     Try {
-      val lines = ArrayBuilder.make[Glucose]
-      val errors = ArrayBuilder.make[InvalidLine]
-      for (line <- Source.fromFile(path, utf8).getLines) {
+      val lines = mutable.ArrayBuilder.make[Glucose]
+      val errors = mutable.ArrayBuilder.make[InvalidLine]
+      val source = Source.fromFile(path, utf8)
+      for (line <- source.getLines) {
         val columns = line.split(delimiter).map(_.trim)
         Glucose.validate(columns) match {
           case Left(error) => errors += InvalidLine(line, error)
           case Right(glucose) => lines += glucose
         }
       }
+      source.close()
       (lines.result(), errors.result())
     }
 
   def csvToMeds(path: String, delimiter: String = ","): Try[(Array[Med], Array[InvalidLine])] =
     Try {
-      val lines = ArrayBuilder.make[Med]
-      val errors = ArrayBuilder.make[InvalidLine]
-      for (line <- Source.fromFile(path, utf8).getLines) {
+      val lines = mutable.ArrayBuilder.make[Med]
+      val errors = mutable.ArrayBuilder.make[InvalidLine]
+      val source = Source.fromFile(path, utf8)
+      for (line <- source.getLines) {
         val columns = line.split(delimiter).map(_.trim)
         Med.validate(columns) match {
           case Left(error) => errors += InvalidLine(line, error)
           case Right(med) => lines += med
         }
       }
+      source.close()
       (lines.result(), errors.result())
     }
 
