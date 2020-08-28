32b84d723d4b9d219731b481d5e6228f49b537ba
objektwerks <objektwerks@runbox.com>
2020-08-28 08:16:10 -0400

    initial impl of glucose-meds chart

--- src/main/scala/objektwerks/chart/GlucoseMedsChart.scala
+++ src/main/scala/objektwerks/chart/GlucoseMedsChart.scala
@@ -1,8 +1,22 @@
 package objektwerks.chart
 
+import java.text.DecimalFormat
+import java.text.SimpleDateFormat
+
 import org.jfree.chart.ChartPanel
 import org.jfree.data.time.TimeSeries
 import org.jfree.data.time.TimeSeriesCollection
+import org.jfree.chart.renderer.xy.XYBarRenderer
+import org.jfree.chart.labels.StandardXYToolTipGenerator
+import org.jfree.chart.plot.XYPlot
+import org.jfree.chart.axis.DateAxis
+import org.jfree.chart.axis.DateTickMarkPosition
+import org.jfree.chart.axis.NumberAxis
+import org.jfree.chart.axis.ValueAxis
+import org.jfree.chart.renderer.xy.XYItemRenderer
+import org.jfree.chart.renderer.xy.StandardXYItemRenderer
+import org.jfree.chart.plot.DatasetRenderingOrder
+import org.jfree.chart.JFreeChart
 
 import scala.util.Success
 import scala.util.Failure
@@ -34,14 +48,26 @@ object GlucoseMedsChart {
     }
 
   private def build(glucoses: Glucoses, meds: Meds): ChartPanel = {
-    val glucoseTimeSeries = buildGlucosesTimeSeries(glucoses)
-    val medTimeSeries = buildMedsTimeSeries(meds)
-    println(glucoseTimeSeries)
-    println(medTimeSeries)
-    new ChartPanel(null)
+    val glucoseTimeSeries = buildGlucoseTimeSeries(glucoses)
+    val glucoseRenderer = buildGlucoseRenderer()
+
+    val dateAxis = buildDateAxis()
+    val valueAxis = buildValueAxis()
+
+    val xyPlot = new XYPlot(glucoseTimeSeries, dateAxis, valueAxis, glucoseRenderer)
+
+    val medTimeSeries = buildMedTimeSeries(meds)
+    val medRenderer = buildMedRenderer()
+
+    xyPlot.setDataset(1, medTimeSeries)
+    xyPlot.setRenderer(1, medRenderer)
+    xyPlot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD)
+  
+    val chart = new JFreeChart("Glucose-Meds Chart", JFreeChart.DEFAULT_TITLE_FONT, xyPlot, true);
+    new ChartPanel(chart)
   }
 
-  private def buildGlucosesTimeSeries(glucoses: Glucoses): TimeSeriesCollection = {
+  private def buildGlucoseTimeSeries(glucoses: Glucoses): TimeSeriesCollection = {
     val timeSeries = new TimeSeries("Glucose")
     glucoses.lines.foreach { glucose =>
       timeSeries.add( glucose.datetime, glucose.level.toDouble )
@@ -49,11 +75,40 @@ object GlucoseMedsChart {
     new TimeSeriesCollection(timeSeries)
   }
 
-  private def buildMedsTimeSeries(meds: Meds): TimeSeriesCollection = {
+  private def buildMedTimeSeries(meds: Meds): TimeSeriesCollection = {
     val timeSeries = new TimeSeries("Meds")
     meds.lines.foreach { med =>
       timeSeries.add( med.datetime, med.dosage.toDouble )
     }
     new TimeSeriesCollection(timeSeries)
   }
+
+  private def buildGlucoseRenderer(): XYItemRenderer = {
+    val renderer = new XYBarRenderer(0.20)
+    val tooltipGenerator = new StandardXYToolTipGenerator( 
+        StandardXYToolTipGenerator.DEFAULT_TOOL_TIP_FORMAT,
+        new SimpleDateFormat("d-MMM-yyyy"),
+        new DecimalFormat("0.00")
+      )
+    renderer.setSeriesToolTipGenerator(0, tooltipGenerator)
+    renderer
+  }
+
+  private def buildMedRenderer(): XYItemRenderer = {
+    val renderer = new StandardXYItemRenderer()
+    val tooltipGenerator = new StandardXYToolTipGenerator(
+      StandardXYToolTipGenerator.DEFAULT_TOOL_TIP_FORMAT,
+      new SimpleDateFormat("d-MMM-yyyy"), new DecimalFormat("0.00")
+    )
+    renderer.setSeriesToolTipGenerator(1, tooltipGenerator)
+    renderer
+  }
+
+  private def buildDateAxis(): DateAxis = {
+    val dateAxis = new DateAxis("Date")
+    dateAxis.setTickMarkPosition(DateTickMarkPosition.MIDDLE)
+    dateAxis
+  }
+
+  private def buildValueAxis(): ValueAxis = new NumberAxis("Value")
 }
