642a2e65e0bc8b7c7551a5d8542d481e3d5093d5
objektwerks <objektwerks@runbox.com>
2020-09-03 15:10:43 -0400

    building out glucose.meds dialog

--- src/main/scala/objektwerks/chart/GlucoseMedsDialog.scala
+++ src/main/scala/objektwerks/chart/GlucoseMedsDialog.scala
@@ -2,28 +2,60 @@ package objektwerks.chart
 
 import java.awt.BorderLayout
 
-import javax.swing.{JButton, JDialog, JFileChooser, JLabel, JPanel}
+import java.awt.event.{ActionEvent, ActionListener}
+
+import javax.swing.{JButton, JDialog, JLabel, JPanel}
 
 import net.miginfocom.swing.MigLayout
 
 class GlucoseMedsDialog(frame: Frame) extends JDialog {
   setTitle(Conf.glucoseMedsDialogTitle)
 
-  val selectLabel = Conf.glucoseMedsDialogSelectButton
-  val glucoseCsvSelectButton = new JButton(selectLabel)
-  val medsCsvSelectButton = new JButton(selectLabel)
-  val selectButton = new JButton(selectLabel)
-
-  val panel = new JPanel(new MigLayout())
-  panel.add( new JLabel(Conf.glucoseCsvLabel) )
-  panel.add( glucoseCsvSelectButton )
-  panel.add( new JLabel(Conf.medsCsvLabel) )
-  panel.add( medsCsvSelectButton )
-  panel.add( selectButton )
+  val panel = buildPanel(Conf.glucoseMedsDialogSelectButton)
   add(panel, BorderLayout.CENTER)
 
   setModal(true)
   setLocationRelativeTo(frame)
   pack()
   setVisible(true)
+
+  private def buildPanel(label: String): JPanel = {
+    new JPanel(new MigLayout())
+    panel.add( new JLabel(Conf.glucoseCsvLabel) )
+    panel.add( buildGlucoseSelectButton(label) )
+    panel.add( new JLabel(Conf.medsCsvLabel) )
+    panel.add( buildMedsSelectButton(label) )
+    panel.add( buildSelectButton(label) )
+    panel
+  }
+
+  private def buildGlucoseSelectButton(label: String): JButton = {
+    val button = new JButton(label)
+    button.addActionListener(new ActionListener() {
+      override def actionPerformed(event: ActionEvent): Unit = {
+        println(event)
+      }
+    })
+    button
+  }
+
+  private def buildMedsSelectButton(label: String): JButton = {
+    val button = new JButton(label)
+    button.addActionListener(new ActionListener() {
+      override def actionPerformed(event: ActionEvent): Unit = {
+        println(event)
+      }
+    })
+    button
+  }
+
+  private def buildSelectButton(label: String): JButton = {
+    val button = new JButton(label)
+    button.addActionListener(new ActionListener() {
+      override def actionPerformed(event: ActionEvent): Unit = {
+        println(event)
+      }
+    })
+    button
+  }
 }
