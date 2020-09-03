ab0c01c1fefdb7a59235c06c68dbe92d65bc90b7
objektwerks <objektwerks@runbox.com>
2020-09-03 13:49:56 -0400

    corrected syntax error

--- src/main/scala/objektwerks/chart/App.scala
+++ src/main/scala/objektwerks/chart/App.scala
@@ -8,7 +8,7 @@ object App {
   def main(args: Array[String]): Unit = {
     EventQueue.invokeLater( new Runnable() {
       override def run(): Unit = {
-        setLookAndFeel(getCrossPlatformLookAndFeelClassName())
+        setLookAndFeel(getCrossPlatformLookAndFeelClassName)
         
         val frame = new Frame()
 
