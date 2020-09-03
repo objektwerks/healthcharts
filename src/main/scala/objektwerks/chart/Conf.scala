91918136f8b8c750a7f2f76ad89bfb94b3c9dd97
objektwerks <objektwerks@runbox.com>
2020-09-03 12:27:49 -0400

    added conf to menubar and action

--- src/main/scala/objektwerks/chart/Conf.scala
+++ src/main/scala/objektwerks/chart/Conf.scala
@@ -4,7 +4,12 @@ import com.typesafe.config.ConfigFactory
 
 object Conf {
   val conf = ConfigFactory.load("chart.conf")
+
   val title = conf.getString("title")
   val width = conf.getInt("width")
   val height = conf.getInt("height")
+
+  val menuBarTitle = conf.getString("menuBarTitle")
+
+  val actionGlucoseMeds = conf.getString("actionGlucoseMeds")
 }
--- src/main/scala/objektwerks/chart/MenuBar.scala
+++ src/main/scala/objektwerks/chart/MenuBar.scala
@@ -3,8 +3,8 @@ package objektwerks.chart
 import javax.swing.{JMenuBar, JMenu, JMenuItem}
 
 class MenuBar() extends JMenuBar() {  
-  val menu = new JMenu("Charts")
-  val menuItem = new JMenuItem( new GlucoseMedsAction("Glucose-Meds") )
+  val menu = new JMenu(Conf.menuBarTitle)
+  val menuItem = new JMenuItem( new GlucoseMedsAction(Conf.actionGlucoseMeds) )
   menu.add(menuItem)
   add(menu)
 }
