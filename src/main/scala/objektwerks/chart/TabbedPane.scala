f158439e261e5798a38e5f3daf6d118395efedb3
objektwerks <objektwerks@runbox.com>
2020-09-06 10:54:04 -0400

    generalized closeable tab constructor

--- src/main/scala/objektwerks/chart/TabbedPane.scala
+++ src/main/scala/objektwerks/chart/TabbedPane.scala
@@ -10,17 +10,17 @@ import org.jfree.chart.ChartPanel
 class TabbedPane extends JTabbedPane {
   def addChart(title: String, chart: ChartPanel): Unit = {
     addTab(null, chart)
-    setTabComponentAt( getTabCount - 1, new CloseableTab(this, title, chart) )
+    setTabComponentAt( getTabCount - 1, new CloseableTab(title, this, chart) )
     setSelectedIndex( indexOfTab(title) )
   }
 }
 
-class CloseableTab(tabbedPane: TabbedPane, title: String, chart: ChartPanel) extends JPanel {
+class CloseableTab(title: String, tabbedPane: JTabbedPane, panel: JPanel) extends JPanel {
   val label = new JLabel(title)
   val closeButton = new JButton("X")
   closeButton.addActionListener( new ActionListener() {
     override def actionPerformed(event: ActionEvent): Unit = {
-      val tabIndex = tabbedPane.indexOfComponent(chart)
+      val tabIndex = tabbedPane.indexOfComponent(panel)
       tabbedPane.removeTabAt(tabIndex)
     }
   })
