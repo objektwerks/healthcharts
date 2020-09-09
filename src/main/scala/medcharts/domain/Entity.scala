9f7de33077275e11bb51366eb7a9c7bbfb0d73f0
objektwerks <objektwerks@runbox.com>
2020-09-09 08:25:17 -0400

    reorganized entities

--- src/main/scala/medcharts/domain/Entity.scala
+++ src/main/scala/medcharts/domain/Entity.scala
@@ -6,6 +6,12 @@ import scala.collection.immutable.SortedMap
 
 final case class Glucose(datetime: Minute, level: Int)
 
+final case class Glucoses(lines: Array[Glucose], invalidLines: Array[InvalidLine])
+
+object Glucoses {
+  def empty: Glucoses = Glucoses(Array.empty[Glucose], Array.empty[InvalidLine])
+}
+
 object MedType extends Enumeration {
   val insulin = Value(1, "insulin")
   val steroid = Value(2, "steroid")
@@ -14,14 +20,7 @@ object MedType extends Enumeration {
 
 final case class Med(datetime: Minute, medtype: MedType.Value, dosage: Int)
 
-final case class Glucoses(lines: Array[Glucose], invalidLines: Array[InvalidLine])
-
-object Glucoses {
-  def empty: Glucoses = Glucoses(Array.empty[Glucose], Array.empty[InvalidLine])
-}
-
 final case class Meds(lines: Array[Med], invalidLines: Array[InvalidLine])
-
 object Meds {
   def empty: Meds = Meds(Array.empty[Med], Array.empty[InvalidLine])
 }
