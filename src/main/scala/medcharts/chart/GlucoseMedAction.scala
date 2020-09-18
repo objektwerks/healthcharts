ef1445fe29e2d90d7eb7adc561b435b4dd766ae4
objektwerks <objektwerks@runbox.com>
2020-09-18 05:22:59 -0400

    refactored conf usage

--- src/main/scala/medcharts/chart/GlucoseMedAction.scala
+++ src/main/scala/medcharts/chart/GlucoseMedAction.scala
@@ -7,19 +7,15 @@ import medcharts.entity._
 import medcharts.ui.{Frame, PathsDialog}
 
 class GlucoseMedAction(name: String, frame: Frame) extends ChartAction(name) {
-  private val title = Conf.titleGlucoseMed
-  private val labelGlucoseCsv = Conf.labelGlucoseCsv
-  private val labelMedCsv = Conf.labelMedCsv
-
   def actionPerformed(event: ActionEvent): Unit = {
-    val paths = new PathsDialog(frame, labelGlucoseCsv, labelMedCsv).view
+    val paths = new PathsDialog(frame, Conf.labelGlucoseCsv, Conf.labelMedCsv).view
     paths match {
       case Some( (glucoseCsvPath, medCsvPath) ) =>
         val glucoses = transformEntities[Glucose](glucoseCsvPath)
         val meds = transformEntities[Med](medCsvPath)
         val chart = GlucoseMedChart.build(glucoses, meds)
         val chartPanel = buildChartPanel(chart)
-        frame.addChart(s"$title-${counter.getAndIncrement}", chartPanel)
+        frame.addChart(s"${Conf.titleGlucoseMed}-${counter.getAndIncrement}", chartPanel)
       case None =>
     }
   }
