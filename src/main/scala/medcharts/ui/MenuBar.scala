fd2ec72d28ebbc0d2b5eaaada388704e123c36a2
objektwerks <objektwerks@runbox.com>
2020-09-18 05:30:29 -0400

    reorganized menu; added separator

--- src/main/scala/medcharts/ui/MenuBar.scala
+++ src/main/scala/medcharts/ui/MenuBar.scala
@@ -1,14 +1,13 @@
 package medcharts.ui
 
-import javax.swing.{JMenu, JMenuBar, JMenuItem}
+import javax.swing.{JMenu, JMenuBar, JMenuItem, JSeparator}
 import medcharts.Conf
 import medcharts.chart.{GlucoseMedAction, WeightAction}
 
 class MenuBar(frame: Frame) extends JMenuBar() {
   val menu = new JMenu(Conf.titleMenuBar)
-  val glucoseMedMenuItem = new JMenuItem( new GlucoseMedAction(Conf.titleGlucoseMed, frame) )
-  val weightMenuItem = new JMenuItem( new WeightAction(Conf.titleWeight, frame) )
-  menu.add(glucoseMedMenuItem)
-  menu.add(weightMenuItem)
+  menu.add( new JMenuItem( new WeightAction(Conf.titleWeight, frame) ) )
+  menu.add( new JSeparator() )
+  menu.add( new JMenuItem( new GlucoseMedAction(Conf.titleGlucoseMed, frame) ) )
   add(menu)
 }
