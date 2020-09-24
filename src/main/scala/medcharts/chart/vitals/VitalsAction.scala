ed2976cb756d9444d3257deaa0a347142ffacf05
objektwerks <objektwerks@runbox.com>
2020-09-24 07:47:57 -0400

    built vitals action

--- src/main/scala/medcharts/chart/vitals/VitalsAction.scala
+++ src/main/scala/medcharts/chart/vitals/VitalsAction.scala
@@ -1,5 +1,22 @@
 package medcharts.chart.vitals
 
-class VitalsAction {
+import java.awt.event.ActionEvent
 
+import medcharts.Conf
+import medcharts.chart.ChartAction
+import medcharts.entity._
+import medcharts.ui.{Frame, PathDialog}
+
+class VitalsAction(name: String, frame: Frame) extends ChartAction(name) {
+  def actionPerformed(event: ActionEvent): Unit = {
+    val path = new PathDialog(frame, Conf.labelVitalsCsv).view
+    path match {
+      case Some( vitalsCsvPath ) =>
+        val vitals = transformEntities[Vitals](vitalsCsvPath)
+        val chart = VitalsChart.build(vitals)
+        val chartPanel = buildChartPanel(chart)
+        frame.addChartPanel(s"${Conf.titleVitals}-${counter.getAndIncrement}", chartPanel)
+      case None =>
+    }
+  }
 }
