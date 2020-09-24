bc5b916468780f78289f6b4c88e26512d42f774f
objektwerks <objektwerks@runbox.com>
2020-09-24 07:50:56 -0400

    added vitals chart menu item to menu bar

--- src/main/scala/medcharts/ui/MenuBar.scala
+++ src/main/scala/medcharts/ui/MenuBar.scala
@@ -9,10 +9,13 @@ import medcharts.chart.pulse.PulseAction
 import medcharts.chart.pulseoxygen.PulseOxygenAction
 import medcharts.chart.respiration.RespirationAction
 import medcharts.chart.temperature.TemperatureAction
+import medcharts.chart.vitals.VitalsAction
 import medcharts.chart.weight.WeightAction
 
 class MenuBar(frame: Frame) extends JMenuBar() {
   val menu = new JMenu(Conf.titleMenuBar)
+  menu.add( new JMenuItem( new VitalsAction(Conf.titleVitals, frame) ) )
+  menu.add( new JSeparator() )
   menu.add( new JMenuItem( new BloodPressureAction(Conf.titleBloodPressure, frame) ) )
   menu.add( new JMenuItem( new PulseAction(Conf.titlePulse, frame) ) )
   menu.add( new JMenuItem( new PulseOxygenAction(Conf.titlePulseOxygen, frame) ) )
