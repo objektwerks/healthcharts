5a82330cbd2852803fd2d12bdd44cc2304a96a39
objektwerks <objektwerks@runbox.com>
2020-09-25 16:54:13 -0400

    reorganized action imports

--- src/main/scala/medcharts/action/WeightAction.scala
+++ src/main/scala/medcharts/action/WeightAction.scala
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent
 import java.util.concurrent.atomic.AtomicInteger
 
 import javax.swing.AbstractAction
+
 import medcharts.Conf
 import medcharts.chart.WeightChart
 import medcharts.entity._
