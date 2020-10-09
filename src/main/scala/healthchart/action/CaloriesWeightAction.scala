a8697f4172be05efd3367ba96a9e138c71e804af
objektwerks <objektwerks@runbox.com>
2020-10-09 11:39:25 -0400

    added calories weight action

---
+++ src/main/scala/healthchart/action/CaloriesWeightAction.scala
@@ -0,0 +1,29 @@
+package healthchart.action
+
+import java.awt.event.ActionEvent
+import java.util.concurrent.atomic.AtomicInteger
+
+import javax.swing.AbstractAction
+
+import healthchart.Conf
+import healthchart.chart.CaloriesWeightChart
+import healthchart.entity.Transformer._
+import healthchart.entity._
+import healthchart.panel.ChartPanelBuilder
+import healthchart.ui.{Frame, PathDialog}
+
+class CaloriesWeightAction(name: String, frame: Frame) extends AbstractAction(name) {
+  protected val counter = new AtomicInteger(1)
+
+  def actionPerformed(event: ActionEvent): Unit = {
+    val path = new PathDialog(frame, Conf.labelCaloriesWeightCsv).view
+    path match {
+      case Some( caloriesWeightCsvPath ) =>
+        val caloriesWeights = transformEntities[CaloriesWeight](caloriesWeightCsvPath)
+        val chart = CaloriesWeightChart.build(caloriesWeights)
+        val chartPanel = ChartPanelBuilder.build(chart, caloriesWeights)
+        frame.addCompositeChartPanel(s"${Conf.titleVitals}-${counter.getAndIncrement}", chartPanel)
+      case None =>
+    }
+  }
+}
