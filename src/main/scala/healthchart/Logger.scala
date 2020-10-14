8a5da24393a71c9bcb61eb423408c78d30c36d68
objektwerks <objektwerks@runbox.com>
2020-10-14 08:37:40 -0400

    changed Logger type IL to IE

--- src/main/scala/healthchart/Logger.scala
+++ src/main/scala/healthchart/Logger.scala
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory
 object Logger {
   private val logger = LoggerFactory.getLogger(getClass)
   
-  def logEntitiesAndInvalidEntities[E, IL](entities: Array[E], invalidEntities: Array[IL]): Unit = {
+  def logEntitiesAndInvalidEntities[E, IE](entities: Array[E], invalidEntities: Array[IE]): Unit = {
     logger.info(s"*** Entities [${entities.length}]: ${entities.toList.map(entity => "\n" + entity.toString)}")
     logger.info(s"*** Invalid Entities [${invalidEntities.length}]: ${invalidEntities.toList.map(line => "\n" + line.toString)}")
   }
