35f6b17c3c0ce7bd4c7fad46afab5ca00dbb41c4
objektwerks <objektwerks@runbox.com>
2021-02-22 14:10:06 -0500

    replaced try with using

--- src/main/scala/healthchart/entity/Transformer.scala
+++ src/main/scala/healthchart/entity/Transformer.scala
@@ -7,17 +7,16 @@ import healthchart.entity.Validator._
 import scala.collection.mutable
 import scala.io.{Codec, Source}
 import scala.reflect.ClassTag
-import scala.util.{Failure, Success, Try}
+import scala.util.{Failure, Success, Try, Using}
 
 object Transformer {
   private val utf8 = Codec.UTF8.name
 
   def transform[E: ClassTag](path: String,
                              delimiter: String = ",")(implicit validator: Validator[E]): Try[Entities[E]] =
-    Try {
+    Using( Source.fromFile(path, utf8) ) { source =>
       val entitiesBuilder = mutable.ArrayBuilder.make[E]
       val invalidEntitiesBuilder = mutable.ArrayBuilder.make[InvalidEntity]
-      val source = Source.fromFile(path, utf8)
       var number = 1
       for (line <- source.getLines()) {
         val columns = line.split(delimiter).map(_.trim)
@@ -27,7 +26,6 @@ object Transformer {
         }
         number += 1
       }
-      source.close()
       val (entities, invalidEntities) = (entitiesBuilder.result(), invalidEntitiesBuilder.result())
       logEntitiesAndInvalidEntities(entities, invalidEntities)
       Entities[E](entities, invalidEntities)
