88b7b30255f413aa0d8ed2021f3ad919b9ddb145
objektwerks <objektwerks@runbox.com>
2021-03-22 14:27:43 -0400

    changed look and feel for apple m1

--- src/main/scala/healthchart/App.scala
+++ src/main/scala/healthchart/App.scala
@@ -10,7 +10,7 @@ object App {
   def main(args: Array[String]): Unit = {
     EventQueue.invokeLater( new Runnable() {
       override def run(): Unit = {
-        setLookAndFeel(getSystemLookAndFeelClassName)
+        setLookAndFeel(getCrossPlatformLookAndFeelClassName)
         val frame = new Frame()
         frame.setVisible(true)
       }
