3a13d41c955f4d7ec8fca03fdf699926bc82a4f7
objektwerks <objektwerks@runbox.com>
2022-04-08 06:43:16 -0400

    reformatted weight action

--- src/main/scala/healthchart/action/WeightAction.scala
+++ src/main/scala/healthchart/action/WeightAction.scala
@@ -12,18 +12,15 @@ import healthchart.entity.Transformer._
 import healthchart.panel.ChartPanelBuilder
 import healthchart.ui.{Frame, PathDialog}
 
-class WeightAction(name: String, frame: Frame) extends AbstractAction(name) {
+class WeightAction(name: String, frame: Frame) extends AbstractAction(name):
   protected val counter = new AtomicInteger(1)
 
-  def actionPerformed(event: ActionEvent): Unit = {
+  def actionPerformed(event: ActionEvent): Unit =
     val path = new PathDialog(frame, Conf.labelWeightCsv).view
-    path match {
+    path match
       case Some( weightCsvPath ) =>
         val weights = transformEntities[Weight](weightCsvPath)
         val chart = WeightChart.build(weights)
         val chartPanel = ChartPanelBuilder.build(chart, weights)
         frame.addCompositeChartPanel(s"${Conf.titleWeight}-${counter.getAndIncrement}", chartPanel)
       case None =>
-    }
-  }
-}
