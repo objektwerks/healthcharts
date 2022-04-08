0b16d5dd1bda70b84416ea15a10f2746a3e3cb45
objektwerks <objektwerks@runbox.com>
2022-04-08 06:44:18 -0400

    reformatted vitals action

--- src/main/scala/healthchart/action/VitalsAction.scala
+++ src/main/scala/healthchart/action/VitalsAction.scala
@@ -12,18 +12,15 @@ import healthchart.entity.Transformer._
 import healthchart.panel.ChartPanelBuilder
 import healthchart.ui.{Frame, PathDialog}
 
-class VitalsAction(name: String, frame: Frame) extends AbstractAction(name) {
+class VitalsAction(name: String, frame: Frame) extends AbstractAction(name):
   protected val counter = new AtomicInteger(1)
 
-  def actionPerformed(event: ActionEvent): Unit = {
+  def actionPerformed(event: ActionEvent): Unit =
     val path = new PathDialog(frame, Conf.labelVitalsCsv).view
-    path match {
+    path match
       case Some( vitalsCsvPath ) =>
         val vitals = transformEntities[Vitals](vitalsCsvPath)
         val chart = VitalsChart.build(vitals)
         val chartPanel = ChartPanelBuilder.build(chart, vitals)
         frame.addCompositeChartPanel(s"${Conf.titleVitals}-${counter.getAndIncrement}", chartPanel)
       case None =>
-    }
-  }
-}
