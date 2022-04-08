4911d7e35ae11565e491e55e4e1e6253ee742f7f
objektwerks <objektwerks@runbox.com>
2022-04-08 05:24:29 -0400

    reformatted tabbed pane

--- src/main/scala/healthchart/ui/TabbedPane.scala
+++ src/main/scala/healthchart/ui/TabbedPane.scala
@@ -4,35 +4,32 @@ import java.awt.event.{MouseAdapter, MouseEvent}
 
 import javax.swing._
 
-class TabbedPane extends JTabbedPane {
-  def addCompositeChartPanel(title: String, chartPanel: JPanel): Unit = {
+class TabbedPane extends JTabbedPane:
+  def addCompositeChartPanel(title: String, chartPanel: JPanel): Unit =
     addTab(null, chartPanel)
     setTabComponentAt( getTabCount - 1, new CloseableTab(title, this, chartPanel) )
     setSelectedIndex( indexOfComponent(chartPanel) )
-  }
-}
 
-class CloseableTab(title: String, tabbedPane: JTabbedPane, panel: JPanel) extends JPanel {
+class CloseableTab(title: String, tabbedPane: JTabbedPane, panel: JPanel) extends JPanel:
   private val closeLabel = new JLabel(title)
   private val closeableLabel = new JLabel("x")
 
   closeableLabel.addMouseListener( new MouseAdapter() {
-    override def mouseEntered(event: MouseEvent): Unit = {
+    override def mouseEntered(event: MouseEvent): Unit =
       val color = closeableLabel.getBackground
       closeableLabel.setBackground(closeableLabel.getForeground)
       closeableLabel.setForeground(color)
-    }
-    override def mouseExited(event: MouseEvent): Unit = {
+
+    override def mouseExited(event: MouseEvent): Unit =
       val color = closeableLabel.getBackground
       closeableLabel.setBackground(closeableLabel.getForeground)
       closeableLabel.setForeground(color)
-    }
-    override def mouseClicked(event: MouseEvent): Unit = {
+
+    override def mouseClicked(event: MouseEvent): Unit =
       tabbedPane.removeTabAt( tabbedPane.indexOfComponent(panel) )
     }
-  })
+  )
 
   setOpaque(false)
   add(closeLabel)
   add(closeableLabel)
-}
