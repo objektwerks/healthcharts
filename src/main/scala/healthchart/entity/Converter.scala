a8a9b61b4aa1977011048a2e5dfb441e565dcf56
objektwerks <objektwerks@runbox.com>
2022-04-08 05:50:48 -0400

    reformatted converter

--- src/main/scala/healthchart/entity/Converter.scala
+++ src/main/scala/healthchart/entity/Converter.scala
@@ -4,8 +4,8 @@ import java.time.LocalDateTime
 
 import org.jfree.data.time.Minute
 
-object Converter {
-  def datetimeToMinute(datetime: String): Minute = {
+object Converter:
+  def datetimeToMinute(datetime: String): Minute =
     val localDateTime = LocalDateTime.parse(datetime)
     new Minute(
       localDateTime.getMinute,
@@ -14,13 +14,10 @@ object Converter {
       localDateTime.getMonthValue,
       localDateTime.getYear
     )
-  }
 
-  def minuteToYearMonthDay(minute: Minute): String = {
+  def minuteToYearMonthDay(minute: Minute): String =
     val hour = minute.getHour
     val year = hour.getYear
     val month = hour.getMonth
     val day = hour.getDayOfMonth
     s"$year.$month.$day"
-  }
-}
