5b2ab77f76917dcd0b934184a5a556271a7d4a58
objektwerks <objektwerks@runbox.com>
2022-04-08 06:15:08 -0400

    reformatted vitals chart

--- src/main/scala/healthchart/chart/VitalsChart.scala
+++ src/main/scala/healthchart/chart/VitalsChart.scala
@@ -17,8 +17,8 @@ import org.jfree.chart.plot.{CombinedDomainXYPlot, PlotOrientation, XYPlot}
 import org.jfree.data.time.{TimeSeries, TimeSeriesCollection}
 import org.jfree.data.xy.XYDataset
 
-object VitalsChart extends Chart {
-  def build(vitals: Entities[Vitals]): JFreeChart = {
+object VitalsChart extends Chart:
+  def build(vitals: Entities[Vitals]): JFreeChart =
     val xAxis = new DateAxis(Conf.titleDayHourChartXAxis)
     xAxis.setDateFormatOverride( new SimpleDateFormat("d,H") )
 
@@ -28,20 +28,18 @@ object VitalsChart extends Chart {
 
     val title = buildTitle(Conf.titleVitals, vitals.toEntity)
     new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, combinedXYPlot, true)
-  }
 
   def buildCombindedXYPlot(xAxis: DateAxis,
                            topXYPlot: XYPlot, 
-                           bottomXYPlot: XYPlot): CombinedDomainXYPlot = {
+                           bottomXYPlot: XYPlot): CombinedDomainXYPlot =
     val combinedXYPlot = new CombinedDomainXYPlot(xAxis)
     combinedXYPlot.setGap(3.0)
     combinedXYPlot.add(topXYPlot, 1)
     combinedXYPlot.add(bottomXYPlot, 1)
     combinedXYPlot.setOrientation(PlotOrientation.VERTICAL)
     combinedXYPlot
-  }
 
-  def buildTopXYPlot(vitals: Entities[Vitals]): XYPlot = {
+  def buildTopXYPlot(vitals: Entities[Vitals]): XYPlot =
     val xyPlot = new XYPlot()
     xyPlot.setBackgroundPaint(Color.LIGHT_GRAY)
 
@@ -58,9 +56,8 @@ object VitalsChart extends Chart {
     xyPlot.setDataset(1, buildTemperatureDataset(vitals))
     xyPlot.setRenderer(1, buildTemperatureRenderer())
     xyPlot
-  }
 
-  def buildBottomXYPlot(vitals: Entities[Vitals]): XYPlot = {
+  def buildBottomXYPlot(vitals: Entities[Vitals]): XYPlot =
     val xyPlot = new XYPlot()
     xyPlot.setBackgroundPaint(Color.LIGHT_GRAY)
 
@@ -80,45 +77,38 @@ object VitalsChart extends Chart {
     xyPlot.setDataset(2, buildOxygenDataset(vitals))
     xyPlot.setRenderer(2, buildOxygenRenderer())
     xyPlot
-  }
 
-  def buildTemperatureDataset(vitals: Entities[Vitals]): XYDataset = {
+  def buildTemperatureDataset(vitals: Entities[Vitals]): XYDataset =
     val timeSeries = new TimeSeries(Conf.titleTemperature)
     vitals.entities.foreach { vital =>
       timeSeries.add( vital.datetime, vital.temperature )
     }
     new TimeSeriesCollection(timeSeries)
-  }
 
-  def buildRespirationDataset(vitals: Entities[Vitals]): XYDataset = {
+  def buildRespirationDataset(vitals: Entities[Vitals]): XYDataset =
     val timeSeries = new TimeSeries(Conf.titleRespiration)
     vitals.entities.foreach { vital =>
       timeSeries.add( vital.datetime, vital.respiration.toDouble )
     }
     new TimeSeriesCollection(timeSeries)
-  }
 
-  def buildPulseDataset(vitals: Entities[Vitals]): XYDataset = {
+  def buildPulseDataset(vitals: Entities[Vitals]): XYDataset =
     val timeSeries = new TimeSeries(Conf.titlePulse)
     vitals.entities.foreach { vital =>
       timeSeries.add( vital.datetime, vital.pulse.toDouble )
     }
     new TimeSeriesCollection(timeSeries)
-  }
 
-  def buildOxygenDataset(vitals: Entities[Vitals]): XYDataset = {
+  def buildOxygenDataset(vitals: Entities[Vitals]): XYDataset =
     val timeSeries = new TimeSeries(Conf.titleOxygen)
     vitals.entities.foreach { vital =>
       timeSeries.add( vital.datetime, vital.oxygen.toDouble )
     }
     new TimeSeriesCollection(timeSeries)
-  }
 
-  def buildBloodPressureDataset(vitals: Entities[Vitals]): XYDataset = {
+  def buildBloodPressureDataset(vitals: Entities[Vitals]): XYDataset =
     val timeSeries = new TimeSeries(Conf.titleSystolic)
     vitals.entities.foreach { vital =>
       timeSeries.add( vital.datetime, s"${vital.systolic}.${vital.diastolic}".toDouble )
     }
     new TimeSeriesCollection(timeSeries)
-  }
-}
