c4f456ce88874531ccdc56f9b45affb03b64be10
objektwerks <objektwerks@runbox.com>
2022-04-08 05:18:23 -0400

    reformatted logger

--- src/main/scala/healthchart/Logger.scala
+++ src/main/scala/healthchart/Logger.scala
@@ -2,14 +2,12 @@ package healthchart
 
 import org.slf4j.LoggerFactory
 
-object Logger {
+object Logger:
   private val logger = LoggerFactory.getLogger(getClass)
   
-  def logEntitiesAndInvalidEntities[E, IE](entities: Array[E], invalidEntities: Array[IE]): Unit = {
+  def logEntitiesAndInvalidEntities[E, IE](entities: Array[E], invalidEntities: Array[IE]): Unit =
     logger.info(s"*** Entities [${entities.length}]: ${entities.toList.map(entity => "\n" + entity.toString)}")
     logger.info(s"*** Invalid Entities [${invalidEntities.length}]: ${invalidEntities.toList.map(line => "\n" + line.toString)}")
-  }
 
   def logFileIOFailure(path: String, failure: Throwable): Unit =
     logger.error(s"*** File IO failure at: $path -> ${failure.getMessage}")
-}
