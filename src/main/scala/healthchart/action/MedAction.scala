48daaa3f1095c6e80127c794d7ea6a59a378f96d
objektwerks <objektwerks@runbox.com>
2023-04-25 12:59:32 -0400

    edited imports

--- src/main/scala/healthchart/action/MedAction.scala
+++ src/main/scala/healthchart/action/MedAction.scala
@@ -2,13 +2,12 @@ package healthchart.action
 
 import java.awt.event.ActionEvent
 import java.util.concurrent.atomic.AtomicInteger
-
 import javax.swing.AbstractAction
 
 import healthchart.Conf
 import healthchart.chart.MedChart
-import healthchart.entity.Transformer._
-import healthchart.entity._
+import healthchart.entity.Transformer.*
+import healthchart.entity.*
 import healthchart.panel.ChartPanelBuilder
 import healthchart.ui.{Frame, PathDialog}
 
