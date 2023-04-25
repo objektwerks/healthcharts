11321a0993b932be405791941b7ef0b745806f36
objektwerks <objektwerks@runbox.com>
2023-04-25 08:13:26 -0400

    edited app

--- src/main/scala/healthchart/App.scala
+++ src/main/scala/healthchart/App.scala
@@ -4,17 +4,16 @@ import com.formdev.flatlaf.FlatLightLaf
 import com.formdev.flatlaf.util.SystemInfo
 
 import java.awt.EventQueue
-
 import javax.swing.UIManager
 
 import healthchart.ui.Frame
 
 object App:
   def main(args: Array[String]): Unit =
-    EventQueue.invokeLater( new Runnable() {
-      override def run(): Unit = {
+    EventQueue.invokeLater(
+      () => {
         UIManager.setLookAndFeel( new FlatLightLaf() )
         val frame = new Frame()
         frame.setVisible(true)
       }
-    })
+    )
