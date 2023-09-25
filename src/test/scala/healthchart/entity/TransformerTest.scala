caff2357f004d1fddbb1eedf56c3b2ad66568ccb
Objektwerks <objektwerks@runbox.com>
2023-09-25 11:34:07 -0400

    Update TransformerTest.scala

--- src/test/scala/healthchart/entity/TransformerTest.scala
+++ src/test/scala/healthchart/entity/TransformerTest.scala
@@ -8,65 +8,53 @@ import scala.util.{Failure, Success}
 
 import healthchart.Logger
 
-class TransformerTest extends AnyFunSuite with Matchers:
-  test("blood pressure") {
+final class TransformerTest extends AnyFunSuite with Matchers:
+  test("blood pressure"):
     testTransformer[BloodPressure]("./data/blood-pressure/blood-pressure.txt", 7, 0)
     testTransformer[BloodPressure]("./data/blood-pressure/blood-pressure-invalid.txt", 5, 2)
-  }
 
-  test("calories weight") {
+  test("calories weight"):
     testTransformer[CaloriesWeight]("./data/calories-weight/calories-weight.txt", 7, 0)
     testTransformer[CaloriesWeight]("./data/calories-weight/calories-weight-invalid.txt", 5, 2)
-  }
 
-  test("glucose") {
+  test("glucose"):
     testTransformer[Glucose]("./data/glucose/glucose.txt", 18, 0)
     testTransformer[Glucose]("./data/glucose/glucose-invalid.txt", 16, 2)
-  }
 
-  test("med") {
+  test("med"):
     testTransformer[Med]("./data/med/med.txt", 18, 0)
     testTransformer[Med]("./data/med/med-invalid.txt", 16, 2)
-  }
 
-  test("glucose med") {
+  test("glucose med"):
     testTransformer[GlucoseMed]("./data/glucose-med/glucose-med.txt", 18, 0)
     testTransformer[GlucoseMed]("./data/glucose-med/glucose-med-invalid.txt", 16, 2)
-  }
 
-  test("pulse") {
+  test("pulse"):
     testTransformer[Pulse]("./data/pulse/pulse.txt", 7, 0)
     testTransformer[Pulse]("./data/pulse/pulse-invalid.txt", 5, 2)
-  }
   
-  test("pulse oxygen") {
+  test("pulse oxygen"):
     testTransformer[PulseOxygen]("./data/pulse-oxygen/pulse-oxygen.txt", 7, 0)
     testTransformer[PulseOxygen]("./data/pulse-oxygen/pulse-oxygen-invalid.txt", 5, 2)
-  }
   
-  test("respiration") {
+  test("respiration"):
     testTransformer[Respiration]("./data/respiration/respiration.txt", 7, 0)
     testTransformer[Respiration]("./data/respiration/respiration-invalid.txt", 5, 2)
-  }
   
-  test("temperature") {
+  test("temperature"):
     testTransformer[Temperature]("./data/temperature/temperature.txt", 7, 0)
     testTransformer[Temperature]("./data/temperature/temperature-invalid.txt", 5, 2)
-  }
 
-  test("vitals") {
+  test("vitals"):
     testTransformer[Vitals]("./data/vitals/vitals.txt", 12, 0)
     testTransformer[Vitals]("./data/vitals/vitals-invalid.txt", 10, 2)
-  }
 
-  test("weight") {
+  test("weight"):
     testTransformer[Weight]("./data/weight/weight.txt", 7, 0)
     testTransformer[Weight]("./data/weight/weight-invalid.txt", 5, 2)
-  }
 
-  test("empty") {
+  test("empty"):
     testTransformer[Vitals]("./data/vitals/vitals-empty.txt", 0, 0)
-  }
 
   private def testTransformer[E: ClassTag](path: String,
                                            entitiesCount: Int,
