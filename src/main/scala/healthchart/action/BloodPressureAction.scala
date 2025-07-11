5a6455ff328abd28ad37dfc899ed270b0eaa00b3
objektwerks <objektwerks@runbox.com>
2025-07-11 19:27:19 -0400

    add final to action

--- src/main/scala/healthchart/action/BloodPressureAction.scala
+++ src/main/scala/healthchart/action/BloodPressureAction.scala
@@ -11,7 +11,7 @@ import healthchart.entity.Transformer.*
 import healthchart.panel.ChartPanelBuilder
 import healthchart.ui.{Frame, PathDialog}
 
-class BloodPressureAction(name: String, frame: Frame) extends AbstractAction(name):
+final class BloodPressureAction(name: String, frame: Frame) extends AbstractAction(name):
   protected val counter = new AtomicInteger(1)
 
   def actionPerformed(event: ActionEvent): Unit =
