6b3e7403c40d35894c6ee732e26449e63645db28
objektwerks <objektwerks@runbox.com>
2025-07-11 19:50:09 -0400

    enhanced error logging

--- src/main/scala/healthchart/action/PulseOxygenAction.scala
+++ src/main/scala/healthchart/action/PulseOxygenAction.scala
@@ -8,6 +8,7 @@ import healthchart.Context
 import healthchart.chart.PulseOxygenChart
 import healthchart.entity.*
 import healthchart.entity.Transformer.*
+import healthchart.Logger.logError
 import healthchart.panel.ChartPanelBuilder
 import healthchart.ui.{Frame, PathDialog}
 
@@ -22,4 +23,4 @@ final class PulseOxygenAction(name: String, frame: Frame) extends AbstractAction
         val chart = PulseOxygenChart.build(pulseoxygens)
         val chartPanel = ChartPanelBuilder.build(chart, pulseoxygens)
         frame.addCompositeChartPanel(s"${Context.titlePulseOxygen}-${counter.getAndIncrement}", chartPanel)
-      case None =>
+      case None => logError(s"*** Path falled: $path")
