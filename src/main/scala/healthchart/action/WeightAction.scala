c03b647d55a4342d10bb43647a0d7989c0835879
objektwerks <objektwerks@runbox.com>
2025-07-11 19:30:27 -0400

    add final to action

--- src/main/scala/healthchart/action/WeightAction.scala
+++ src/main/scala/healthchart/action/WeightAction.scala
@@ -11,7 +11,7 @@ import healthchart.entity.Transformer.*
 import healthchart.panel.ChartPanelBuilder
 import healthchart.ui.{Frame, PathDialog}
 
-class WeightAction(name: String, frame: Frame) extends AbstractAction(name):
+final class WeightAction(name: String, frame: Frame) extends AbstractAction(name):
   protected val counter = new AtomicInteger(1)
 
   def actionPerformed(event: ActionEvent): Unit =
