39130d89c55bfe1049d66709be4f6174980087f6
objektwerks <objektwerks@runbox.com>
2025-07-11 15:50:52 -0400

    added taskbar support

--- src/main/scala/healthchart/App.scala
+++ src/main/scala/healthchart/App.scala
@@ -2,16 +2,25 @@ package healthchart
 
 import com.formdev.flatlaf.FlatLightLaf
 
-import java.awt.EventQueue
+import java.awt.{EventQueue, Taskbar, Toolkit}
+import java.awt.Taskbar.Feature
+
 import javax.swing.UIManager
 
 import healthchart.ui.Frame
 
 object App:
-  def main(args: Array[String]): Unit =  
+  def main(args: Array[String]): Unit =
     EventQueue.invokeLater(
       () => {
         UIManager.setLookAndFeel( FlatLightLaf() )
         Frame().setVisible(true)
+
+        if Taskbar.isTaskbarSupported() then
+          val taskbar = Taskbar.getTaskbar()
+          if taskbar.isSupported(Feature.ICON_IMAGE) then
+            val defaultToolkit = Toolkit.getDefaultToolkit()
+            val appIcon = defaultToolkit.getImage(getClass().getResource("/icon.png"))
+            taskbar.setIconImage(appIcon)
       }
     )
