63f9b45deb6f6aaee941744092ce0d787e4f82fb
objektwerks <objektwerks@runbox.com>
2025-07-11 19:28:40 -0400

    add final to action

--- src/main/scala/healthchart/action/MedAction.scala
+++ src/main/scala/healthchart/action/MedAction.scala
@@ -11,7 +11,7 @@ import healthchart.entity.*
 import healthchart.panel.ChartPanelBuilder
 import healthchart.ui.{Frame, PathDialog}
 
-class MedAction(name: String, frame: Frame) extends AbstractAction(name):
+final class MedAction(name: String, frame: Frame) extends AbstractAction(name):
   protected val counter = new AtomicInteger(1)
 
   def actionPerformed(event: ActionEvent): Unit =
