4fa30af425061eae0750ffb6fd49aab5bce95ce6
objektwerks <objektwerks@runbox.com>
2025-07-11 19:28:08 -0400

    add final to action

--- src/main/scala/healthchart/action/GlucoseAction.scala
+++ src/main/scala/healthchart/action/GlucoseAction.scala
@@ -11,7 +11,7 @@ import healthchart.entity.Transformer.*
 import healthchart.panel.ChartPanelBuilder
 import healthchart.ui.{Frame, PathDialog}
 
-class GlucoseAction(name: String, frame: Frame) extends AbstractAction(name):
+final class GlucoseAction(name: String, frame: Frame) extends AbstractAction(name):
   protected val counter = new AtomicInteger(1)
 
   def actionPerformed(event: ActionEvent): Unit =
