44cf1aedb1467253b551b1e19678ddf91aee020e
objektwerks <objektwerks@runbox.com>
2025-07-11 21:50:43 -0400

    added final to all actions

--- src/main/scala/healthchart/action/ExitAction.scala
+++ src/main/scala/healthchart/action/ExitAction.scala
@@ -3,5 +3,5 @@ package healthchart.action
 import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
 
-class ExitAction(name: String) extends AbstractAction(name):
+final class ExitAction(name: String) extends AbstractAction(name):
   def actionPerformed(event: ActionEvent): Unit = System.exit(0)
--- src/main/scala/healthchart/action/RespirationAction.scala
+++ src/main/scala/healthchart/action/RespirationAction.scala
@@ -12,7 +12,7 @@ import healthchart.Logger.logError
 import healthchart.panel.ChartPanelBuilder
 import healthchart.ui.{Frame, PathDialog}
 
-class RespirationAction(name: String, frame: Frame) extends AbstractAction(name):
+final class RespirationAction(name: String, frame: Frame) extends AbstractAction(name):
   protected val counter = new AtomicInteger(1)
 
   def actionPerformed(event: ActionEvent): Unit =
