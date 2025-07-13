bf730f2714951c6932b73d75fb6b02fffe22172e
objektwerks <objektwerks@runbox.com>
2025-07-13 06:45:43 -0400

    removed new

--- src/main/scala/healthchart/chart/RespirationChart.scala
+++ src/main/scala/healthchart/chart/RespirationChart.scala
@@ -16,35 +16,35 @@ import healthchart.entity.*
 
 object RespirationChart extends Chart:
   def build(respirations: Entities[Respiration]): JFreeChart =
-    val xyPlot = new XYPlot()
+    val xyPlot = XYPlot()
     xyPlot.setDataset( buildRespirationDataset(respirations) )
     xyPlot.setRenderer( buildRespirationRenderer() )
 
-    val xAxis = new DateAxis(Context.titleDayHourChartXAxis)
-    xAxis.setDateFormatOverride( new SimpleDateFormat("d,H") )
+    val xAxis = DateAxis(Context.titleDayHourChartXAxis)
+    xAxis.setDateFormatOverride( SimpleDateFormat("d,H") )
     xyPlot.setDomainAxis(xAxis)
 
-    val yAxis = new NumberAxis(Context.titleRespirationChartYAxis)
+    val yAxis = NumberAxis(Context.titleRespirationChartYAxis)
     xyPlot.setRangeAxis(yAxis)
 
     val title = buildTitle(Context.titleRespiration, respirations.toEntity)
-    new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, xyPlot, true)
+    JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, xyPlot, true)
 
   def buildRespirationDataset(respirations: Entities[Respiration]): XYDataset =
-    val timeSeries = new TimeSeries(Context.titleRespiration)
+    val timeSeries = TimeSeries(Context.titleRespiration)
     respirations.entities.foreach { respiration =>
       timeSeries.add( respiration.datetime, respiration.breathesPerMinute.toDouble )
     }
-    new TimeSeriesCollection(timeSeries)
+    TimeSeriesCollection(timeSeries)
 
   def buildRespirationRenderer(): XYItemRenderer =
-    val renderer = new XYLineAndShapeRenderer()
+    val renderer = XYLineAndShapeRenderer()
     val tooltipGenerator = new StandardXYToolTipGenerator() {
       override def generateToolTip(dataset: XYDataset, series: Int, item: Int): String =
         val xValue = dataset.getXValue(series, item)
         val yValue = dataset.getYValue(series, item)
-        val dayHourMinute = new SimpleDateFormat("d,H:m").format( new jdate.Date( xValue.toLong ) )
-        val breathesPerMinute = new DecimalFormat("0").format( yValue )
+        val dayHourMinute = SimpleDateFormat("d,H:m").format( jdate.Date( xValue.toLong ) )
+        val breathesPerMinute = DecimalFormat("0").format( yValue )
         val delta = calculateDeltaAsPercentage(dataset, series, item)
         s"${Context.titleRespiration}: ($dayHourMinute, $breathesPerMinute, $delta%)"
       override def clone() = this
