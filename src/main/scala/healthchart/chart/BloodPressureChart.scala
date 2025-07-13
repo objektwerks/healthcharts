87bea1bcbb3a19718843b05baf84aac061ec8f16
objektwerks <objektwerks@runbox.com>
2025-07-13 06:38:40 -0400

    removed new

--- src/main/scala/healthchart/chart/BloodPressureChart.scala
+++ src/main/scala/healthchart/chart/BloodPressureChart.scala
@@ -16,37 +16,37 @@ import healthchart.entity.{BloodPressure, Entities}
 
 object BloodPressureChart extends Chart:
   def build(bloodpressures: Entities[BloodPressure]): JFreeChart =
-    val xyPlot = new XYPlot()
+    val xyPlot = XYPlot()
     xyPlot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD)
 
     xyPlot.setDataset(0, buildBloodPressureDataset(bloodpressures))
     xyPlot.setRenderer(0, buildBloodPressureRenderer())
 
-    val xAxis = new DateAxis(Context.titleDayHourChartXAxis)
-    xAxis.setDateFormatOverride( new SimpleDateFormat("d,H") )
+    val xAxis = DateAxis(Context.titleDayHourChartXAxis)
+    xAxis.setDateFormatOverride( SimpleDateFormat("d,H") )
     xyPlot.setDomainAxis(0, xAxis)
 
-    val yAxis = new NumberAxis(Context.titleBloodPressureChartYAxis)
+    val yAxis = NumberAxis(Context.titleBloodPressureChartYAxis)
     xyPlot.setRangeAxis(yAxis)
 
     val title = buildTitle(Context.titleBloodPressure, bloodpressures.toEntity)
-    new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, xyPlot, true)
+    JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, xyPlot, true)
 
   def buildBloodPressureDataset(bloodpressures: Entities[BloodPressure]): XYDataset =
-    val timeSeries = new TimeSeries(Context.titleSystolic)
+    val timeSeries = TimeSeries(Context.titleSystolic)
     bloodpressures.entities.foreach { bloodpressure =>
       timeSeries.add( bloodpressure.datetime, s"${bloodpressure.systolic}.${bloodpressure.diastolic }".toDouble )
     }
-    new TimeSeriesCollection(timeSeries)
+    TimeSeriesCollection(timeSeries)
 
   def buildBloodPressureRenderer(): XYItemRenderer =
-    val renderer = new XYLineAndShapeRenderer()
+    val renderer = XYLineAndShapeRenderer()
     val tooltipGenerator = new StandardXYToolTipGenerator() {
       override def generateToolTip(dataset: XYDataset, series: Int, item: Int): String =
         val xValue = dataset.getXValue(series, item)
         val yValue = dataset.getYValue(series, item)
         val yValues = yValue.toString.split("\\.")
-        val dayHourMinute = new SimpleDateFormat("d,H:m").format( new jdate.Date( xValue.toLong ) )
+        val dayHourMinute = SimpleDateFormat("d,H:m").format( jdate.Date( xValue.toLong ) )
         val systolic = yValues(0)
         var diastolic = yValues(1)
         if (diastolic.length == 1) diastolic = diastolic + 0  // Hack! DecimalFormat dropping trailing zero!
